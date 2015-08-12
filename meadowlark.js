c750cc4d69ad2970ada1e1654beb9d3b8e8c24ea
Danilo Lima <danlima69@gmail.com>
2015-08-11 23:34:52 -0400

    Modified server

--- meadowlark.js
+++ meadowlark.js
@@ -1,30 +1,30 @@
 var express = require('express');
 var app = express();
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 8080);
 
 app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    res.render('about');
 });
 
 // Custom 404 page
 app.use(function(req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not found.');
+    res.render('404');
 });
 
 // Custom 500 page
 app.use(function(req, res) {
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server error.');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
